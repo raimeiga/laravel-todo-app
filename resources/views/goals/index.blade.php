38969fbfdffd219902a2ef865e9f8929d8dc2f15
raimeiga <yusuke612@hotmail.com>
2023-10-29 18:52:30 +0900

    10.3 トップページのビューを編集/ToDoの追加用ボタン

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -35,13 +35,17 @@
  
                  <!-- 目標の削除用モーダル -->
                  @include('modals.delete_goal')  
+
+                 <!-- ToDoの追加用モーダル -->
+                 @include('modals.add_todo')  
  
                  <div class="col">     
                      <div class="card bg-light">
                          <div class="card-body d-flex justify-content-between align-items-center">
                              <h4 class="card-title ms-1 mb-0">{{ $goal->title }}</h4>  <!-- 更新・削除するデータ goalsテーブルのtitleカラム -->
                              <div class="d-flex align-items-center">                                 
-                                 <div class="dropdown">
+                                   <a href="#" class="px-2 fs-5 fw-bold link-dark text-decoration-none" data-bs-toggle="modal" data-bs-target="#addTodoModal{{ $goal->id }}">＋</a>    
+                                  <div class="dropdown">
                                      <a href="#" class="dropdown-toggle px-1 fs-5 fw-bold link-dark text-decoration-none menu-icon" id="dropdownGoalMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">︙</a>                                     
                                      <ul class="dropdown-menu dropdown-menu-end text-center" aria-labelledby="dropdownGoalMenuLink">
                                                <!-- 編集・削除リンク（目標の編集用・削除用モーダルを開くためのリンク）
