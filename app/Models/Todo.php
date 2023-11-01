981a14a76b185b34ee8f959a0229cfcf83b2a302
raimeiga <yusuke612@hotmail.com>
2023-11-01 14:37:48 +0900

    13.6 todosテーブルとtagsテーブルのリレーションシップを設定

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -15,5 +15,9 @@ class Todo extends Model
 
      public function goal() {
          return $this->belongsTo(Goal::class); //← goalsテーブルとのリレーションシップを設定
-     }    
+     }   
+     
+     public function tags() {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }    
 }
